ac63ee5f748f6999bd92a93ee073ee7a231b8a17
bunyaminsensoy <105174590+bunyaminsensoy@users.noreply.github.com>
2022-05-31 22:40:12 +0300

    Bünyamin Güncelleme

--- Hastane/RandevuSistemi.py
+++ Hastane/RandevuSistemi.py
@@ -1,3 +1,5 @@
+from numpy import choose
+
 import pandas as pd # awesome python kutuphanesi zorunlu oldugu icin pandasi kullandik.
 from hastane import Hastane # menu ve hastandeki verileri cekebilmek icin birlikte kullandik.
 
@@ -5,7 +7,15 @@ class GirisEkrani():   #giris ekrani olusturmak icin bir class tanimladik.
     def __init__(self):
         print("Randevu sistemine hos geldiniz")
         self.oturumAcma = False #acildgi gibi calismamasi icin false olarak atandi
+        self.cycle = True
         self.kayit = Hastane()
+        self. doctors = ["Hakan Tasiyan", "Muslum Gurses", "Esengul", "Bergen"]
+        self.disease = ["Agiz Dis Cene Cerrahisi", "Deri ve Zuhrevi", "Goz hastaliklari", "Noroloji"]
+        self.vaccine = ["Turcovac","Biontech","Sinovac"]
+        
+        self.doctorsDisease = {"Diseases": self.disease, "Doctors": self.doctors}
+
+
 
     def randevuAl(self):
         cycle = True # while dongusune girebilmesi icin True olarak atama yaptik.
@@ -37,17 +47,56 @@ class GirisEkrani():   #giris ekrani olusturmak icin bir class tanimladik.
             print()
 
     def giris(self): # giris ekraninin calismasi icin ggiris fonksiyonu calistirildi.
-        pass
+        user = input("Hastanin tc'sini giriniz: ").strip()          # Guvenlik kontrolune gerek yok
+        password = input("Hastanin soyismini giriniz: ").strip()    # Guvenlik kontrolune gerek yok
+
+        if user in self.kayit.recordList.keys():
+            if password == self.kayit.recordList[user]:
+                print("Sisteme basariyla giris yapildi!")
+                self.oturumAcma = True
+            else:
+                print("Sifre yanlis girildi!!")
+        else:
+            print("Bu kayitta bir kullanici bulunamadi!")
+        print()
 
     def yeniKayit(self): # yeni kayit ekraninin calismasi icin yeni kayit fonksiyonu calistirildi.
-        pass
-
+        self.kayit.hastaKayit()
     def hastalikSecimi(self): # hastalik secimi ekraninin  ccalismasi icin hastalik secimi fonksiyonu calistirildi. 
-        pass
+        if self.oturumAcma == True:
+            print("Doktor randevusu mu asi randevus mu?")
+            choose = input("Asi icin : 1\nDoktor randevusu icin : 2\nSeçiminizi Giriniz : ").strip()
+            while not choose.isnumeric():
+                choose = input("Gecerli formatta bir secim giriniz: ")
+            if choose == '2':
+                print("\nHastalik ve Listesi : \n")
+                df = pd.DataFrame.from_dict(self.doctorsDisease)
+                print(df.to_string())
+                secim = input("\nSecmek istediginiz hastaligin kodunu girin: ").strip()
+                while not secim.isnumeric() or int(secim) < 0 or int(secim) > 4:
+                    secim = input("Alan kodu en soldaki sutundaki sayisal degerlerdir tekrar deneyiniz: "),
+                print("Randevu bilgileri : ")
+                df_1 = df.iloc[int(secim)]
+                print(df_1.to_string())
+                print("\nRandevunuz basariyla olusturuldu! Gecmis olsun.")
+                self.cycle = False
+
+            if choose == '1':
+                print()
+                sr = pd.Series(self.vaccine)
+                print(sr.to_string(),end='\n')
+                secim = input("\nSecmek istediginiz asinin kodunu girin: ").strip()
+                while not secim.isnumeric() or int(secim) < 0 or int(secim) > 3:
+                    secim = input("Alan kodu en soldaki sutundaki sayisal degerlerdir tekrar deneyiniz: "),
+                sr2 = sr.iloc[int(secim)]
+                print("Randevu bilgileri : ")
+                print(sr2)
+                self.cycle = False
+                print("\nRandevunuz basariyla olusturuldu! Gecmis olsun.")
+        else:
+            print("Islem yapmadan once oturum aciniz!!")
+
 
     def logOut(self):
         print("Oturum kapatiliyor!! Gule gule") # logOut(cikis) ekraninin  calismasi icin logOut fonksiyonu calistirildi.
         self.oturumAcma = False
-
-    def guncel(self):
-        pass
