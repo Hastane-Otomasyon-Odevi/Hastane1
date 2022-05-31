3b5599bbe6e5774865d20fb39d2570c5e5c6f013
ssinemozen <67108388+ssinemozen@users.noreply.github.com>
2022-05-31 15:21:53 +0300

    sinem güncelleme

--- Hastane/RandevuSistemi.py
+++ Hastane/RandevuSistemi.py
@@ -1,28 +1,26 @@
 import pandas as pd # awesome python kutuphanesi zorunlu oldugu icin pandasi kullandik.
 from hastane import Hastane # menu ve hastandeki verileri cekebilmek icin birlikte kullandik.
 
-class GirisEkrani():   #
+class GirisEkrani():   #giris ekrani olusturmak icin bir class tanimladik.
     def __init__(self):
         print("Randevu sistemine hos geldiniz")
-        self.oturumAcma = False
+        self.oturumAcma = False #acildgi gibi calismamasi icin false olarak atandi
         self.kayit = Hastane()
-
-        self.doctorsDisease = {"Diseases": self.disease, "Doctors": self.doctors}
 
     def randevuAl(self):
         cycle = True # while dongusune girebilmesi icin True olarak atama yaptik.
-        while cycle:
-            print("Giris yapmak icin : 1")
-            print("Kayit olmak icin : 2")
-            print("Randevu almak icin : 3")
-            print("Logout icin : 4")
-            print("Cikmak icin : 5")
-            print("Bizi seçtiginiz icin tesekkürler")
-            choose = int(input("Yapmak istediginiz islem :  "))
+        while cycle: #cycle atadigimiz degerden dolayi while dongusune dondu.
+            print("Giris yapmak icin : 1") # giriş yapmak için 1 rakamına basılır.
+            print("Kayit olmak icin : 2")  # kayıt olmak için 2 rakamına basılır.
+            print("Randevu almak icin : 3")# randevu almak için 3 rakamına basılır.
+            print("Logout icin : 4")       # hasta oturumunu sonlandırmak için 4 rakamına basılır
+            print("Cikmak icin : 5")       # çıkış yapmak için 5 rakamına basılır.
+            print("Bizi seçtiginiz icin tesekkürler")  # ekrana küçük bir bilgilendirme notu ekledik.
+            choose = int(input("Yapmak istediginiz islem :  ")) # yapmak istediği işlemi seçmek için input ve choose'a kayıt aldık.
 
             if self.oturumAcma == True and choose == 1:
                 print("Yeni bir oturum acmadan once oturumunuzu kapatiniz.")
-            if choose == 1 and (not self.oturumAcma):
+            if choose == 1 and (not self.oturumAcma): 
                 self.giris()
             if choose == 2:
                 self.yeniKayit()
