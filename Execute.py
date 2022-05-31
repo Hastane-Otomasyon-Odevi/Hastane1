7d277d0249a904bf23ca6fbf8159b9fb93e809b1
bunyaminsensoy <105174590+bunyaminsensoy@users.noreply.github.com>
2022-05-31 19:26:02 +0300

    Bünyamin Güncelleme

---
+++ Execute.py
@@ -0,0 +1,9 @@
+"""
+Otomasyonu Çalıştıracak Olan Kod Kısmı
+"""
+
+from RandevuSistemi import GirisEkrani # randevu sisteminden giriş ekranına import sağlandı
+
+ekran = GirisEkrani() # degisken ataması 
+
+ekran.randevuAl() # ekran için fonksiyon kullanımı
