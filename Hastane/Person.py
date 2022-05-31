2a4794d6640e79496980d61dd4797eb01700b24e
Meisterschaft <100562780+Meisterschaft@users.noreply.github.com>
2022-05-31 22:00:00 +0300

    Kurtuluş yorum satırı düzenleme

--- Hastane/Person.py
+++ Hastane/Person.py
@@ -1,13 +1,13 @@
-class Person:
-    def __init__(self, tc, soyisim):
-        self.__tc = tc
-        self.__soyisim = soyisim
+class Person:   #Person class'ı tanımlandı
+    def __init__(self, tc, soyisim):   #tc ve soyisim parametreleri istendi.
+        self.__tc = tc   #self ile tc ataması yapıldı.
+        self.__soyisim = soyisim   #self ile soyisim ataması yapıldı.
 
 
-    @property
-    def tc(self):
+    @property   #getter setter metodu kullanıldı.
+    def tc(self):   
         return self.__tc
 
-    @property
+    @property   #getter setter metodu kullanıldı.
     def soyisim(self):
         return self.__soyisim
