e8b7674034bcb3f710a1c67d4629fab71dfd1b48
Meisterschaft <100562780+Meisterschaft@users.noreply.github.com>
2022-05-31 15:28:30 +0300

    Kurtuluş Güncelleme

--- Hastane/hastane.py
+++ Hastane/hastane.py
@@ -1,19 +1,19 @@
 import pandas as pd   #Awesome Python kütüphanesinin kullanımı zorunlu olduğu için pandas'ı kullandık.
-from Person import Person    #Person'dan veri çekebilmek için kullandık.
+from Person import Person   #Person'dan veri çekebilmek için kullandık.
 
-class Hastane():
+class Hastane():   #Hastane class'ı oluşturuldu.
     def __init__(self):
         print("Hastanemize hosgeldiniz")
         self.__recordList = {}
 
-    def menuSecim(self):
-        cycle = True
+    def menuSecim(self):   #Menü seçim fonksiyonu oluşturuldu
+        cycle = True   #While döngüsüne girebilmesi için True değeri atandı.
 
-        while cycle:
+        while cycle:   #While döngüsüne girebilmesi için cycle'daki değer değerlendirildi.
             try:
-                secim = input("ekle : 1\nsil : 2\ngoster : 3\nLutfen yapmak istediginiz islemi secin: ").strip()
-                while not (secim.isnumeric()) or int(secim) > 4 or int(secim) < 1:
-                    secim = input("Gecersiz bir komut girildi, tekrar deneyin: ").strip()
+                secim = input("ekle : 1\nsil : 2\ngoster : 3\nLutfen yapmak istediginiz islemi secin: ").strip()   #Seçim yapılabilmesi için input değer alındı, .strip metoduyla boşluklar silindi.
+                while not (secim.isnumeric()) or int(secim) > 4 or int(secim) < 1:   #Seçimin sadece numaralardan ve istenilen aralıkta olması istendi ve while ile döngü kontrolü yapıldı.
+                    secim = input("Gecersiz bir komut girildi, tekrar deneyin: ").strip()   
             except Exception as e:
                 print("Oops! ", e.__class__, " hatasi meydana geldi!")
             if secim == '1':
